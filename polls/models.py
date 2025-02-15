263eb38b7bbc1b2d7838b52348a997404bdb731a
Lain Iwakura <klabweb@protonmail.com>
2025-02-15 12:57:29 -0500

    create migration for added Choice and Question models

--- polls/models.py
+++ polls/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # columns are represented by a Field, class, which defines DB column data type
 
 class Question(models.Model):
-    question_text = models.charField(max_length=200)
+    question_text = models.CharField(max_length=200)
     # pub_date has a "human readable" note attached to it. Useful for API docs.
     pub_date = models.DateTimeField("date published")
     
