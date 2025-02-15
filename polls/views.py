b424a3d07ad6c84c2c0920d1a91ad189c02caf2b
Lain Iwakura <klabweb@protonmail.com>
2025-02-15 15:53:26 -0500

    update polls index view to actually render Postgres data, instead of stub message, with entity details hyperlinked to "detail" path s

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,14 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.template import loader
+from django.shortcuts import render
+from .models import Question
 
-# each function is a view. Each view is a model business behavior for the model's url interface.
+# each function is a view. Each view is a model behavior mapped to the model's url interface. Standard Path -> Function.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_questions_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_questions_list": latest_questions_list}
+    return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
     return HttpResponse(f"You're looking at question {question_id}")
