afba882dffbd08de50b578067dd69f7bbf2bba62
Lain Iwakura <klabweb@protonmail.com>
2025-02-15 15:08:52 -0500

    add additional polls views, this time with path param inputs, as well as urlpattern path endpoints mapped to them

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,16 @@
 from django.http import HttpResponse
 
+# each function is a view. Each view is a model business behavior for the model's url interface.
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}")
+
+def results(request, question_id):
+    response = f"You're looking at the results of {question_id}"
+    return HttpResponse(response)
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}")
