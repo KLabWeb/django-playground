2b551651b2abc4e760105a07e060ed209d84db29
Lain Iwakura <klabweb@protonmail.com>
2025-02-15 12:56:18 -0500

    define Question and Choice models

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# models represents a table in a DB
+# columns are represented by a Field, class, which defines DB column data type
+
+class Question(models.Model):
+    question_text = models.charField(max_length=200)
+    # pub_date has a "human readable" note attached to it. Useful for API docs.
+    pub_date = models.DateTimeField("date published")
+    
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)     
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    
